34f3d1e496f27a8518a10c618c603dcfa805f816
Vivian T <150468584+bluesberries@users.noreply.github.com>
2024-08-29 14:07:47 -0700

    Add func to decode query response

--- greptile_apis.go
+++ greptile_apis.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
@@ -12,6 +13,10 @@ import (
 var greptileApiKey = os.Getenv("GREPTILE_API_KEY")
 var githubToken = os.Getenv("GITHUB_TOKEN")
 
+/**
+ * This function was based on code from
+ * https://docs.greptile.com/quickstart
+ */
 func query() {
 	queryData := map[string]interface{}{
 		"messages": []map[string]string{
@@ -37,10 +42,52 @@ func query() {
 	queryReq.Header.Set("Content-Type", "application/json")
 
 	queryClient := &http.Client{}
-	queryResp, _ := queryClient.Do(queryReq)
+	queryResp, err := queryClient.Do(queryReq)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer queryResp.Body.Close()
 
-	body, _ := io.ReadAll(queryResp.Body)
-	bodyString := string(body)
-	fmt.Println(bodyString)
+	decodeMessage(queryResp.Body)
+}
+
+type Source struct {
+	Repository string
+	Remote     string
+	Branch     string
+	FilePath   string
+	LineStart  int
+	LineEnd    int
+	Summary    string
+}
+
+type Message struct {
+	Sources []Source
+}
+
+/**
+ * This function was based on code from
+ * https://dev.to/taqkarim/you-might-not-be-using-json-decoder-correctly-in-golang-12mb
+ */
+func decodeMessage(respBody io.ReadCloser) {
+	decoder := json.NewDecoder(respBody)
+	for {
+		var m Message
+		if err := decoder.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		for i, source := range m.Sources {
+			fmt.Printf("sources[%d]\n", i)
+			fmt.Printf("repository:\t%s\n", source.Repository)
+			fmt.Printf("remote:\t\t%s\n", source.Remote)
+			fmt.Printf("branch:\t\t%s\n", source.Branch)
+			fmt.Printf("filepath:\t%s\n", source.FilePath)
+			fmt.Printf("linestart:\t%d\n", source.LineStart)
+			fmt.Printf("lineend:\t%d\n", source.LineEnd)
+			fmt.Printf("summary:\t%s\n\n", source.Summary)
+		}
+	}
 }
