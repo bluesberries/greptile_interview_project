fef4d591472cba4963269d7e27f0dcea7bef7e76
Vivian T <150468584+bluesberries@users.noreply.github.com>
2024-08-29 14:34:50 -0700

    Add func for Greptile search API

--- greptile_apis.go
+++ greptile_apis.go
@@ -48,7 +48,7 @@ func query() {
 	}
 	defer queryResp.Body.Close()
 
-	decodeMessage(queryResp.Body)
+	decodeQueryMessage(queryResp.Body)
 }
 
 type Source struct {
@@ -61,7 +61,7 @@ type Source struct {
 	Summary    string
 }
 
-type Message struct {
+type QueryMessage struct {
 	Sources []Source
 }
 
@@ -69,10 +69,10 @@ type Message struct {
  * This function was based on code from
  * https://dev.to/taqkarim/you-might-not-be-using-json-decoder-correctly-in-golang-12mb
  */
-func decodeMessage(respBody io.ReadCloser) {
-	decoder := json.NewDecoder(respBody)
+func decodeQueryMessage(queryRespBody io.ReadCloser) {
+	decoder := json.NewDecoder(queryRespBody)
 	for {
-		var m Message
+		var m QueryMessage
 		if err := decoder.Decode(&m); err == io.EOF {
 			break
 		} else if err != nil {
@@ -88,6 +88,73 @@ func decodeMessage(respBody io.ReadCloser) {
 			fmt.Printf("linestart:\t%d\n", source.LineStart)
 			fmt.Printf("lineend:\t%d\n", source.LineEnd)
 			fmt.Printf("summary:\t%s\n\n", source.Summary)
+		}
+	}
+}
+
+func search() {
+	searchData := map[string]interface{}{
+		"messages": []map[string]string{
+			{
+				"id":      "some-id-1",
+				"content": "Functions taht use recursion",
+				"role":    "user",
+			},
+		},
+		"repositories": []map[string]string{
+			{
+				"repository": "bluesberries/redis-greptile-assessment",
+				"branch":     "unstable",
+			},
+		},
+		"sessionId": "test-session-id", // optional
+	}
+	searchPayload, _ := json.Marshal(searchData)
+
+	searchReq, _ := http.NewRequest("POST", "https://api.greptile.com/v2/search", bytes.NewBuffer(searchPayload))
+	searchReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", greptileApiKey))
+	searchReq.Header.Set("X-Github-Token", githubToken)
+	searchReq.Header.Set("Content-Type", "application/json")
+
+	searchClient := &http.Client{}
+	searchResp, err := searchClient.Do(searchReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer searchResp.Body.Close()
+
+	decodeSearchMessage(searchResp.Body)
+}
+
+type SearchMessage struct {
+	Repository string
+	Remote     string
+	Branch     string
+	FilePath   string
+	LineStart  int
+	LineEnd    int
+	Summary    string
+}
+
+func decodeSearchMessage(searchRespBody io.ReadCloser) {
+	decoder := json.NewDecoder(searchRespBody)
+	for {
+		var messages []SearchMessage
+		if err := decoder.Decode(&messages); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		for i, m := range messages {
+			fmt.Printf("[%d]\n", i)
+			fmt.Printf("repository:\t%s\n", m.Repository)
+			fmt.Printf("remote:\t\t%s\n", m.Remote)
+			fmt.Printf("branch:\t\t%s\n", m.Branch)
+			fmt.Printf("filepath:\t%s\n", m.FilePath)
+			fmt.Printf("linestart:\t%d\n", m.LineStart)
+			fmt.Printf("lineend:\t%d\n", m.LineEnd)
+			fmt.Printf("summary:\t%s\n\n", m.Summary)
 		}
 	}
 }
