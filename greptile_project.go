7831a83586556e6a07c8c39827649ea39c7f8514
Vivian T <150468584+bluesberries@users.noreply.github.com>
2024-08-30 16:58:38 -0700

    Clean up messages

--- greptile_project.go
+++ greptile_project.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi! I list files that may contain POTENTIAL issues.")
+	fmt.Printf("Hi! I list files that may contain POTENTIAL issues.")
 
 	nl := "nested_loop"
 	r := "recursion"
@@ -15,8 +15,8 @@ func main() {
 	queries[r] = "Functions that use recursion"
 
 	responses := make(map[string]string)
-	responses[nl] = "+++Nested loops: Potentially inefficient algorithms+++"
-	responses[r] = "+++Recursion: Potentially excessive recursion (e.g. stack overflow, poor performance from deep recursions)+++"
+	responses[nl] = "*** Nested loops: Potentially inefficient algorithms ***"
+	responses[r] = "*** Recursion: Potentially excessive recursion (e.g. stack overflow, poor performance from deep recursions) ***"
 
 	for key := range queries {
 		processAndDisplaySearch(queries[key], responses[key])
@@ -25,7 +25,7 @@ func main() {
 
 func processAndDisplaySearch(query, userMsg string) {
 	responses := search(query)
-	fmt.Println(userMsg)
+	fmt.Printf("\n\n%s\n\n", userMsg)
 	for _, resp := range responses {
 		fmt.Println(resp.FilePath)
 	}
